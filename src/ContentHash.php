ce08a05264dece26a933261a84708f54f8d236e1
Simon R Jones <simon@studio24.net>
2020-02-03 23:59:05 +0000

    Refactoring to store an array of content hashes

--- src/ContentHash.php
+++ src/ContentHash.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 namespace Strata\Data;
 
 use Strata\Data\Exception\InvalidHashAlgorithm;
+use Strata\Data\Exception\InvalidHashIdentifier;
 
+/**
+ * A simple content hash class to help check whether content has been updated
+ *
+ * @package Strata\Data
+ */
 class ContentHash
 {
     /** @var string */
@@ -13,6 +19,20 @@ class ContentHash
     /** @var string */
     protected $algorithm;
 
+    /**
+     * Array of ID => Content hashes
+     *
+     * @var array
+     */
+    protected $content = [];
+
+    /**
+     * Constructor
+     *
+     * @param string|null $algorithm Algorithm to use when using hash() function
+     * @throws InvalidHashAlgorithm
+     * @see https://www.php.net/hash
+     */
     public function __construct(string $algorithm = null)
     {
         if (null !== $algorithm) {
@@ -26,6 +46,85 @@ class ContentHash
         }
     }
 
+    public function __sleep()
+    {
+        // TODO: Implement __sleep() method.
+    }
+
+    public function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+    /**
+     * Compare whether the content is new or updated
+     *
+     * @param string|int $id Identifier for this content
+     * @param string $content
+     * @return bool Whether the content is new or updated
+     * @throws InvalidHashIdentifier
+     */
+    public function isChanged($id, string $content): bool
+    {
+        $this->validateIdentifier($id);
+
+        // Check whether content exists and is identical
+        if (isset($this->content[$id]) && ($this->hash($content) === $this->content[$id])) {
+            return false;
+        }
+
+        // Otherwise it's new or changed
+        $this->add($id, $content);
+        return true;
+    }
+
+    /**
+     * Add or replace content
+     *
+     * @param string|int $id Identifier for this content
+     * @param string $content Content
+     * @throws InvalidHashIdentifier
+     */
+    public function add($id, string $content)
+    {
+        $this->validateIdentifier($id);
+        $this->content[$id] = $this->hash($content);
+    }
+
+    /**
+     * Remove content
+     *
+     * @param $id
+     * @throws InvalidHashIdentifier
+     */
+    public function remove($id)
+    {
+        $this->validateIdentifier($id);
+        if (isset($this->content[$id])) {
+            unset($this->content[$id]);
+        }
+    }
+
+    /**
+     * Validate whether the identifier argument is OK
+     *
+     * @param $id
+     * @throws InvalidHashIdentifier
+     */
+    protected function validateIdentifier($id)
+    {
+        static $results = [];
+        if (isset($results[$id])) {
+            return;
+        }
+
+        if (!is_string($id) && !is_int($id)) {
+            throw new InvalidHashIdentifier(sprintf('$id argument must be string or integer, %s passed', gettype($id)));
+        }
+
+        $results[$id] = true;
+    }
+
     /**
      * Return a hash based on passed content
      *
@@ -37,15 +136,4 @@ class ContentHash
         return hash($this->algorithm, $content);
     }
 
-    /**
-     * Compare two hashes to see if they are identical
-     *
-     * @param string $hash1
-     * @param string $hash2
-     * @return bool
-     */
-    public function compare(string $hash1, string $hash2): bool
-    {
-        return ($hash1 === $hash2);
-    }
 }
--- tests/ContentHashTest.php
+++ tests/ContentHashTest.php
@@ -12,27 +12,15 @@ final class ContentHashTest extends TestCase
 
     public function testContentHash()
     {
-        $content1 = <<<EOD
-May your coming year be filled with magic and dreams and good madness. I hope you read some fine books and kiss someone 
-who thinks you're wonderful, and don't forget to make some art - write or draw or build or sing or live as only you can. 
-And I hope, somewhere in the next year, you surprise yourself.
-
-EOD;
-
-        $content2 = <<<EOD
-Some words are here that are not the same as the words above by the fine Neil Gaiman. These words are different. 
-
-EOD;
-
         $contentHash = new ContentHash();
-        $a = $contentHash->hash($content1);
-        $b = $contentHash->hash($content2);
-        $c = $contentHash->hash($content1);
-        $d = $contentHash->hash(trim($content1));
+        foreach ($this->getContent() as $key => $val) {
+            $contentHash->add($key, $val);
+        }
 
-        $this->assertFalse($contentHash->compare($a, $b));
-        $this->assertTrue($contentHash->compare($a, $c));
-        $this->assertFalse($contentHash->compare($a, $d));
+        $this->assertFalse($contentHash->isChanged(0, "Some words."));
+        $this->assertFalse($contentHash->isChanged(1, "Some words that are different to the ones that went before."));
+        $this->assertFalse($contentHash->isChanged(4, "And now for something completely different."));
+        $this->assertTrue($contentHash->isChanged(4, " And now for something completely different. "));
     }
 
     public function testInvalidHash()
@@ -43,22 +31,24 @@ EOD;
 
     public function testDifferentHash()
     {
-        $content1 = <<<EOD
-Some words that are different to the ones that went before.
-
-EOD;
-
-        $content2 = <<<EOD
-More words that are different again. 
-
-EOD;
-
         $contentHash = new ContentHash('md5');
-        $a = $contentHash->hash($content1);
-        $b = $contentHash->hash($content2);
-        $c = $contentHash->hash($content1);
+        foreach ($this->getContent() as $key => $val) {
+            $contentHash->add($key, $val);
+        }
 
-        $this->assertFalse($contentHash->compare($a, $b));
-        $this->assertTrue($contentHash->compare($a, $c));
+        $this->assertFalse($contentHash->isChanged(0, "Some words."));
+        $this->assertFalse($contentHash->isChanged(1, "Some words that are different to the ones that went before."));
     }
+
+    protected function getContent(): array
+    {
+        return [
+            "Some words.",
+            "Some words that are different to the ones that went before.",
+            "More words that are different again.",
+            "Another string slightly longer than before.",
+            "And now for something completely different.",
+        ];
+    }
+
 }
